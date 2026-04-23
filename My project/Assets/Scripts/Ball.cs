8fc50b14a561326fe597b4edd6f632e917d2c6c2
jp273934 <jparris195@gmail.com>
2026-04-23 16:44:28 -0500

    launch ball

---
+++ My project/Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public int moveSpeed;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        moveSpeed = 200;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb.linearVelocity = Vector2.up * moveSpeed * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // transform.Translate(Vector2.up * moveSpeed * Time.deltaTime); 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.name == "Paddle")
+        {
+            Debug.Log("Paddle was hit");
+            rb.linearVelocity = new Vector2(1, 1) * moveSpeed * Time.deltaTime;
+        }
+
+       if(collision.gameObject.name == "Ceiling")
+        {
+
+        }
+    }
+}
